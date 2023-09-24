f9b80fac73baedcec6638ec8b68a87e74972195a
HadiqaAziz <hadiqa.aziz@gmail.com>
2023-09-24 12:47:14 -0700

    include order.js ans user.js in model

---
+++ Develop/models/Order.js
@@ -0,0 +1,40 @@
+const {Model, DataTypes} = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Order extends Model {}
+
+Order.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        order_status: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        order_date: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            validate: {
+                isDate: true
+            }
+        },
+       
+        customer_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        } 
+    },
+    {
+        sequelize,
+        modelName: 'order'
+    }
+);
+
+module.exports = Order;
