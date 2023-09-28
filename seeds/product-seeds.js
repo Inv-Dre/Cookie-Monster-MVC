f9af5f8dd06a9581dbf5a2bb3c2e560b4bc7881f
Dre Tay <andretaylor681@gmail.com>
2023-09-28 02:30:03 -0400

    added path to login page on sign up button, changed product pics and product names to a link to a page wiith just that one item, made a new parrtial for the lone product its the same as the original partial just removed the page link.

--- seeds/product-seeds.js
+++ seeds/product-seeds.js
@@ -27,42 +27,49 @@ const productData = [
         price: 39.99,
         stock: 20,
         category_id: 2,
+        filename:"Bloom.png"
     },
     {
         product_name: 'NATURELO One Daily Multivitamin',
         price: 59.95,
         stock: 17,
         category_id: 2,
+        filename:"Naturelo.png"
     },
     {
         product_name: 'Anker USB-C Cable',
         price: 9.99,
         stock: 27,
         category_id: 1,
+        filename:"Anker.png"
     },
     {
         product_name: 'ATERCEL Workout Gloves',
         price: 13.95,
         stock: 35,
         category_id: 2,
+        filename:"Atercel.png"
     },
     {
         product_name: 'Oridium Adjustable Dumbbell',
         price: 219.99,
         stock: 9,
         category_id: 2,
+        filename:"oridium-adjustable-dumbbell.png"
     },
     {
         product_name: 'Wireless 3 in 1 Charging Station',
         price: 27.19,
         stock: 19,
         category_id: 1,
+        filename:"wireless-charging-station.png"
     },
     {
         product_name: 'WD Elements 5TB Prtable HDD',
         price: 104.09,
         stock: 4,
         category_id: 1,
+        filename:"WD-Elements-5TB-Prtable-HDD.png"
     }
 ];
 
