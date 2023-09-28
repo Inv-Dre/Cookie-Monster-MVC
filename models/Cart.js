c08dfa1fcfddf6f40ed17f5efe0c29c37866ed9f
HadiqaAziz <hadiqa.aziz@gmail.com>
2023-09-27 21:17:59 -0700

    include cart.js in models

---
+++ models/Cart.js
@@ -0,0 +1,73 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Cart extends Model {}
+
+Cart.init(
+    {
+        items: [
+            {
+              productId: {
+                type: DataTypes.OBJECT,
+                references: {
+                    model: "product",
+                    key: "id",
+                },
+                
+              },
+              qty: {
+                type: Number,
+                default: 0,
+              },
+              price: {
+                type: Number,
+                default: 0,
+              },
+              title: {
+                type: String,
+              },
+              productCode: {
+                type: String,
+              },
+            },
+        ],
+    
+        totalQty: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          
+        },
+        totalCost: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            required: true,
+        
+        },
+        user: {
+          type: DataTypes.OBJECT,
+          allowNull: false,
+          references: {
+            model: "user",
+          
+          },
+          required: true,
+        },
+        createdAt: {
+            type: DataTypes.Date,
+            allowNull: false,
+            default: Date.now,
+        },
+        
+        
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: "category",
+    }
+    
+);
+
+module.exports = Cart;
